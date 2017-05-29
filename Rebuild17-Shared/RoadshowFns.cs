e51a0af9e4c0038eaebb27c65cb12dbadca39102
Mike Benkovich <mike@benko.com>
2017-05-29 08:48:22 -0500

    Add project files.

---
+++ Rebuild17-Shared/RoadshowFns.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rebuild17_Shared
+{
+    public class RoadshowFns
+    {
+        public string GetRoadshowName()
+        {
+            return "re-BUILD 2017 Roadshow";
+        }
+    }
+}
