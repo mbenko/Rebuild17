955c6e96163cf35e76ca1cf342f3d3c1a211e289
Mike Benkovich <mike@benko.com>
2017-06-12 13:11:45 -0500

    Add UI for Portland

--- MyRoadshowApp/MyRoadshowApp/MyData.cs
+++ MyRoadshowApp/MyRoadshowApp/MyData.cs
@@ -14,23 +14,24 @@ namespace MyRoadshowApp
         public MyDataViewModel()
         {
             Title = "re-BUILD 2017 Roadshow";
-            Events = new ObservableCollection<Event>();
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/22", ImageURL = "stlouis.jpg", Venue = "St Louis" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/23", ImageURL = "kansascity.jpg", Venue = "Kansas City" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/24", ImageURL = "omaha.jpg", Venue = "Omaha" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/25", ImageURL = "desmoines.jpg", Venue = "Des Moines" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/26", ImageURL = "minneapolis.png", Venue = "Minneapolis" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "5/31", ImageURL = "chicago.jpg", Venue = "Austin" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/1", ImageURL = "background.jpg", Venue = "Chicago" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/2", ImageURL = "chicago.jpg", Venue = "Milwaukee" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/5", ImageURL = "chicago.jpg", Venue = "New York" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/7", ImageURL = "chicago.jpg", Venue = "Tampa" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/9", ImageURL = "chicago.jpg", Venue = "Atlanta" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/12", ImageURL = "chicago.jpg", Venue = "Portland" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/14", ImageURL = "chicago.jpg", Venue = "San Diego" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/15", ImageURL = "chicago.jpg", Venue = "Phoenix" });
-            Events.Add(new Event { Name = "VS 2017 Best Of Launch", Date = "6/16", ImageURL = "chicago.jpg", Venue = "Denver" });
-
+            Events = new ObservableCollection<Event>
+            {
+                new Event { Name = "reBuild 2017", Date = "5/22", ImageURL = "stlouis.jpg", Venue = "St Louis" },
+                new Event { Name = "reBuild 2017", Date = "5/23", ImageURL = "kansascity.jpg", Venue = "Kansas City" },
+                new Event { Name = "reBuild 2017", Date = "5/24", ImageURL = "omaha.jpg", Venue = "Omaha" },
+                new Event { Name = "reBuild 2017", Date = "5/25", ImageURL = "desmoines.jpg", Venue = "Des Moines" },
+                new Event { Name = "reBuild 2017", Date = "5/26", ImageURL = "minneapolis.png", Venue = "Minneapolis" },
+                new Event { Name = "reBuild 2017", Date = "5/31", ImageURL = "chicago.jpg", Venue = "Austin" },
+                new Event { Name = "reBuild 2017", Date = "6/1", ImageURL = "background.jpg", Venue = "Chicago" },
+                new Event { Name = "reBuild 2017", Date = "6/2", ImageURL = "chicago.jpg", Venue = "Milwaukee" },
+                new Event { Name = "reBuild 2017", Date = "6/5", ImageURL = "chicago.jpg", Venue = "New York" },
+                new Event { Name = "reBuild 2017", Date = "6/7", ImageURL = "chicago.jpg", Venue = "Tampa" },
+                new Event { Name = "reBuild 2017", Date = "6/9", ImageURL = "chicago.jpg", Venue = "Atlanta" },
+                new Event { Name = "reBuild 2017", Date = "6/12", ImageURL = "chicago.jpg", Venue = "Portland" },
+                new Event { Name = "reBuild 2017", Date = "6/14", ImageURL = "chicago.jpg", Venue = "San Diego" },
+                new Event { Name = "reBuild 2017", Date = "6/15", ImageURL = "chicago.jpg", Venue = "Phoenix" },
+                new Event { Name = "reBuild 2017", Date = "6/16", ImageURL = "chicago.jpg", Venue = "Denver" }
+            };
         }
     }
     public class Event
