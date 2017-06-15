194239c0e4deff2da4db521d283c8e53763666a3
Mike Benkovich <mike@benko.com>
2017-06-15 13:45:06 -0500

    not preview core

--- MyRoadshowApp/MyRoadshowApp/App.xaml.cs
+++ MyRoadshowApp/MyRoadshowApp/App.xaml.cs
@@ -33,10 +33,10 @@ namespace MyRoadshowApp
 		protected override void OnStart ()
 		{
             // Handle when your app starts
-            MobileCenter.Start("android=46bb8944-268e-4fcd-9eb0-5d2f5d08072f;" +
-                   "uwp={Your UWP App secret here};" +
-                   "ios={Your iOS App secret here}",
-                   typeof(Analytics), typeof(Crashes));
+            MobileCenter.Start("android=9cff0fb0-5591-4bac-807a-3fbbb5e71983;" +
+                               "uwp={Your UWP App secret here};" +
+                               "ios={Your iOS App secret here}",
+                               typeof(Analytics), typeof(Crashes));
         }
 
 		protected override void OnSleep ()
